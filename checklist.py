710710419db6d402766b13b0cd757d133f082275
Lucila Hernandez <lucilahernandez@Lucilas-MacBook-Pro.local>
2024-09-20 13:36:29 -0700

    implemented while loops

--- checklist.py
+++ checklist.py
@@ -37,35 +37,58 @@ def mark_completed(index):
     else:
         print("Index out of range.")
 
-def select(function_code):
-    # Create item
-    if function_code == "C":
-        input_item = user_input("Input item:")
-        create(input_item)
-
-    # Read item
-    elif function_code == "R":
-        item_index = user_input("Index Number?")
-
-        # Remember that item_index must actually exist or our program will crash.
-        read(item_index)
-
-    # Print all items
-    elif function_code == "P":
-        list_all_items()
-
-    # Catch all
-    else:
-        print("Unknown Option")
-
 def user_input(prompt):
     # the input function will display a message in the terminal
     # and wait for user input.
     user_input = input(prompt)
     return user_input
 
+def select(function_code):
+    function_code = function_code.upper()
+    # Create item
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+        print("Item added.")
 
+    # Read item
+    elif function_code == "R":
+        try:
+            item_index = int(user_input("Index Number?"))
+            print(read(item_index))
+        # Remember that item_index must actually exist or our program will crash.
+        except ValueError:
+            print("Please enter a valid integer.")
 
+    elif function_code == "U":
+        try:
+            item_index = int(user_input("Index Number?"))
+            new_item = user_input("New item:")
+            update(item_index, new_item)
+            print("Item updated.")
+        except ValueError:
+            print("Please enter a valid integer.")
+    
+    elif function_code == "D":
+        try:
+            item_index = int(user_input("Index Number?"))
+            destroy(item_index)
+            print("Item destroyed.")
+        except ValueError:
+            print("Please enter a valid integer.")
+
+    # Print all items
+    elif function_code == "P":
+        list_all_items()
+    
+    elif function_code == "Q":
+    # This is where we want to stop our loop
+        return False
+
+    # Catch all
+    else:
+        print("Unknown Option")
+    return True
 
 
 def test():
@@ -80,23 +103,24 @@ def test():
     destroy(1)
 
     print(read(0))
-#print(read(1))
+
     list_all_items()
-    # Call your new function with the appropriate value
+  
     select("C")
-    # View the results
+    
     list_all_items()
-    # Call function with new value
+    
     select("R")
-    # View results
+    
     list_all_items()
-    # Continue until all code is run
+    
     user_value = user_input("Please Enter a value:")
     print(user_value)
 test()
 
+running = True
+while running:
+    selection = user_input(
+        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
+    running = select(selection)
 
-
-#checklist = ['Blue', 'Orange']
-#checklist[1] = 'Cats'
-#print(checklist)
